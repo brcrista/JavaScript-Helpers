5b59141c93bbcbd5a30d9bae68a7cd2b81b49395
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-07-01 16:24:58 -0400

    Add toYield

--- test/iterable.test.js
+++ test/iterable.test.js
@@ -21,6 +21,43 @@ expect.extend({
         return {
             pass: true
         };
+    },
+
+    toYield(received, ...expectedValues) {
+        if (!iterable.isIterable(received)) {
+            return {
+                message: () => `${received} is not iterable`,
+                pass: false
+            };
+        }
+
+        const actual = Array.from(received);
+
+        if (actual.length > expectedValues.length) {
+            return {
+                message: () => `${actual} has more elements (${actual.length}) than ${expectedValues} (${expectedValues.length})`,
+                pass: false
+            };
+        } else if (actual.length < expectedValues.length) {
+            return {
+                message: () => `${actual} has fewer elements (${actual.length}) than ${expectedValues} (${expectedValues.length})`,
+                pass: false
+            };
+        }
+
+        // `actual` and `expectedValues` have the same length
+        for (let i = 0; i < actual.length; i++) {
+            if (actual[i] !== expectedValues[i]) {
+                return {
+                    message: () => `${actual} and ${expectedValues} differ at index ${i} (${actual[i]} !== ${expectedValues[i]})`,
+                    pass: false
+                };
+            }
+        }
+
+        return {
+            pass: true
+        };
     }
 });
 
@@ -58,7 +95,27 @@ describe('iterable.chain', () => {
         expect(iterable.chain()).toBeEmptyIterable();
     });
 
+    // test('returns an empty iterable when nothing is chained', () => {
+    //     expect(iterable.chain()).toYield([]);
+    // });
+
+    test('passes through a single argument', () => {
+        expect(iterable.chain([1, 2])).toYield(1, 2);
+    });
+
     // test('passes through a single argument', () => {
-    //     expect(iterable.chain([1, 2])).toYield([1, 2]);
+    //     expect(iterable.chain([1, 2, 3])).toYield(1, 2);
+    // });
+
+    // test('passes through a single argument', () => {
+    //     expect(iterable.chain([1, 2, 3])).toYield(1, 3, 3);
+    // });
+
+    // test('passes through a single argument', () => {
+    //     expect(iterable.chain([1, 2])).toYield(1, 3, 3);
+    // });
+
+    // test('passes through a single argument', () => {
+    //     expect(iterable.chain([1, 2])).toYield();
     // });
 });
