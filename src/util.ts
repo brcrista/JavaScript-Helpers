1aa5f3b8e22a748fd8fdbc125a1cc7bfdfa18fea
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-07-01 18:43:03 -0400

    Refactor the `util` array functions to use iterablesf

--- src/util.ts
+++ src/util.ts
@@ -2,22 +2,19 @@
  * Produce an array of `n` items from a `sequencer` function.
  * The `sequencer` function will be passed the index of each element being generated.
  */
-export function sequence<T>(n: number, sequencer: (i: number) => T): T[] {
-    const result: T[] = [];
+export function* sequence<T>(n: number, sequencer: (i: number) => T) {
     for (let i = 0; i < n; i++) {
-        result.push(sequencer(i));
+        yield sequencer(i);
     }
-
-    return result;
 }
 
 /**
- * Generate an array of integers from `min` to `max` (inclusive).
- * If `min` is not an integer it is rounded up.
- * If `max` is not an integer it is rounded down.
+ * Generate an iterable of integers from `min` to `max` (inclusive).
+ * If `min` is not an integer, it is rounded up.
+ * If `max` is not an integer, it is rounded down.
  * If `min` > `max` (after rounding if `min` or `max` is not an integer), an empty array is returned.
  */
-export function range(min: number, max: number): number[] {
+export function range(min: number, max: number) {
     min = Math.ceil(min);
     max = Math.floor(max);
 
@@ -27,8 +24,7 @@ export function range(min: number, max: number): number[] {
 
 /** Remove the `i`th element from an array and return it. */
 export function remove<T>(array: T[], i: number): T {
-    if (!(i in range(0, array.length - 1)))
-    {
+    if (!(i in range(0, array.length - 1))) {
         throw new RangeError(`${i} is not in the range [0, ${array.length})`);
     }
 
@@ -36,12 +32,12 @@ export function remove<T>(array: T[], i: number): T {
 }
 
 /** Randomly choose an integer in the range [`0`, `size`) with a uniform distribution. */
-export function random(size: number) {
+export function random(size: number): number {
     return Math.floor(Math.random() * size);
 }
 
-/** Create a random permutation of an array. */
-export function shuffle<T>(array: T[]): T[] {
-    const arrayCopy = array.slice(0);
-    return sequence(array.length, () => remove(arrayCopy, random(arrayCopy.length)));
+/** Create a random permutation of an iterable. */
+export function shuffle<T>(iterable: Iterable<T>) {
+    const arrayCopy = Array.from(iterable);
+    return sequence(arrayCopy.length, () => remove(arrayCopy, random(arrayCopy.length)));
 }
